75285ae3f26a7d156cddf98861681a3b75f38d95
Obulareddy.Reddy <Obulareddy.Reddy@yodlee.com>
2022-05-09 22:53:49 +0530

    code clean

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import ProfileIntro from '../projects/profile/intro'
 import Portfolio from '../projects/profile/portfolio'
 import MyTechLifeStack from '../projects/profile/mytechlife'
 import ProfileTechStack from '../projects/profile/techstack'
-import styles from '../styles/Home.module.css'
-import QuoteOne from '../projects/profile/quotes/_quote1'
-import Footer from '../projects/profile/footer'
 
 export default function Home() {
   return (
--- projects/profile/quotes/index.js
+++
@@ -1,5 +0,0 @@
-import QuoteOne from "./_quote1";
-
-export {
-    QuoteOne
-}
