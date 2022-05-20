30d72599ae961a7c85c7725bc84478a19af32bae
Obulareddy.Reddy <Obulareddy.Reddy@yodlee.com>
2022-05-20 09:50:29 +0530

    fixed vercel build issues

--- projects/blogger/oauth/index.js
+++ projects/blogger/oauth/index.js
@@ -3,40 +3,40 @@ import MtlLogo from '../../../components/common/logo';
 
 const GoogOAuth = ({ onGoogleSignIn }) => {
     return (
-        <div class="hero min-h-screen bg-base-200">
-            <div class="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
 
-                <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div class="card-body">
-                        <div class="flex flex-col items-center justify-center text-center lg:text-left">
-                            <h1 class="text-5xl font-bold">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="flex flex-col items-center justify-center text-center lg:text-left">
+                            <h1 className="text-5xl font-bold">
                                 <MtlLogo fontSize="text-2xl" />
                             </h1>
-                            <p class="py-6 font-RobotoSlab">A product-focused javascript fullstack developer based in Bangalore. I specialise in creating interactive experiences and functional interfaces using React, Canvas, Atomic Architecture and Design Systems. <span className="font-bold">This is an admin gateway for my blogger & youtube posts.</span></p>
+                            <p className="py-6 font-RobotoSlab">A product-focused javascript fullstack developer based in Bangalore. I specialise in creating interactive experiences and functional interfaces using React, Canvas, Atomic Architecture and Design Systems. <span className="font-bold">This is an admin gateway for my blogger & youtube posts.</span></p>
                         </div>
-                        <div class="flex flex-col w-full border-opacity-50">
-                            <div class="h-fit w-full">
-                                <div class="form-control">
-                                    <label class="label">
-                                        <span class="label-text">Email</span>
+                        <div className="flex flex-col w-full border-opacity-50">
+                            <div className="h-fit w-full">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
                                     </label>
-                                    <input type="text" placeholder="email" class="input input-bordered" />
+                                    <input type="text" placeholder="email" className="input input-bordered" />
                                 </div>
-                                <div class="form-control">
-                                    <label class="label">
-                                        <span class="label-text">Password</span>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
                                     </label>
-                                    <input type="text" placeholder="password" class="input input-bordered" />
-                                    <label class="label">
-                                        <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                                    <input type="text" placeholder="password" className="input input-bordered" />
+                                    <label className="label">
+                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                                     </label>
                                 </div>
-                                <div class="form-control mt-6">
-                                    <button class="btn btn-primary">Login</button>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Login</button>
                                 </div>
                             </div>
-                            <div class="divider">OR</div>
-                            <div class="h-fit w-full">
+                            <div className="divider">OR</div>
+                            <div className="h-fit w-full">
                                 <button onClick={onGoogleSignIn} className="w-full">
                                     <div className="flex text-white bg-[#4285f4] border-r-2 shadow-xl relative w-full h-[46px] rounded">
                                         <div className="m-1 bg-white w-[40px] h-[40px]">
